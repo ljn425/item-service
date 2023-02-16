d2ce8cd1c4dacfb18d4680de7fb3f0c5afd9b38b
ljn425 <ljn425@naver.com>
2023-02-16 20:21:26 +0900

    상품도메인 개발

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,24 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.Repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+
+        return "";
+    }
+
+
+}
